86b551d548e7695bb12f75b785f1185578365744
Quimey <quimeymata@gmail.com>
2023-04-11 19:05:06 -0300

    Adds validation logic with Validatable interface

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,49 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength !== undefined &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid &&
+      validatableInput.value.trim().length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength !== undefined &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid &&
+      validatableInput.value.trim().length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min !== undefined &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max !== undefined &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // autobind decorator
 function autobind(
   _target: any,
@@ -56,11 +102,28 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
-    ) {
+    const enteredTitleIsValid = validate({
+      value: enteredTitle,
+      required: true,
+    });
+
+    const enteredDescriptionIsValid = validate({
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    });
+
+    const enteredPeopleIsValid = validate({
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    });
+
+    const inputIsValid =
+      enteredTitleIsValid && enteredDescriptionIsValid && enteredPeopleIsValid;
+
+    if (!inputIsValid) {
       alert('Invalid input, please try again!');
       return;
     } else {
